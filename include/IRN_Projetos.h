80ff1db4bca86042ea283f6a1a4bece38dbe2e5b
miguel-amf <miguel.ccunb@gmail.com>
2016-12-01 18:38:38 -0200

    Adicionado stub da IRN_Projetos pela classe MRN_Projetos

--- include/IRN_Projetos.h
+++ include/IRN_Projetos.h
@@ -1,31 +1,23 @@
 #ifndef IRN_PROJETOS_H
 #define IRN_PROJETOS_H
 
+#include <stdexcept>
+#include <Projeto.h>
 
 class IRN_Projetos
 {
     public:
-        virtual void allProjetos() throw (runtime_error) = 0;
+        virtual void allProjetos() throw(runtime_error) = 0;
 
-        virtual Projeto getProjeto(CodigoProjeto) throw (runtime_error) = 0;
-        virtual Projeto getProjeto(Nome) throw (runtime_error) = 0;
+        virtual Projeto getProjeto(CodigoProjeto) throw(runtime_error) = 0;
+        virtual Projeto getProjeto(Nome) throw(runtime_error) = 0;
 
-        /*carrega um projeto para a classe, afim de ser editado ou removido*/
-        virtual bool carregarProjeto(CodigoProjeto) throw (runtime_error) = 0;
-        virtual Projeto carregarProjeto(Nome) throw (runtime_error) = 0;
+        virtual void coutProjeto(CodigoProjeto) throw (runtime_error) = 0;
+        virtual void coutProjeto(Nome) throw (runtime_error) = 0;
 
-        /*Exclui da persistencia tambem*/
-        virtual bool removeProjetoCarregado() throw (runtime_error) = 0;
+        virtual bool fecharProjeto(CodigoProjeto) throw (runtime_error) = 0;
 
-        virtual bool modificarProjetoCarregado(Nome);
-        virtual bool modificarProjetoCarregado(CodigoProjeto);
-        virtual bool modificarProjetoCarregado(GerenteProjeto);
-        /*char: 'a' para adicionar e 'r' para remover*/
-        virtual bool modificarProjetoCarregado(Desenvolvedor, char);
-        /*char: 'a' para atual, 'p' para previsto*/
-        virtual bool modificarProjetoCarregado(Custo, char);
-        /*char: 'i' para inicio, 't' para termino*/
-        virtual bool modificarProjetoCarregado(Data, char);
+        virtual bool editarProjeto(CodigoProjeto, Projeto) throw (runtime_error) = 0;
 
     protected:
     private:
